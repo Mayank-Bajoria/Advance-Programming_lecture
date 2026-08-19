cc6231e3baab2a760b364bfb45fc75b9b9524ccf
mayankbajoria <bajoriamayank2007@gmail.com>
2026-08-19 09:31:22 +0530

    Require module

---
+++ script.js
@@ -0,0 +1,5 @@
+const time  = '9 am';
+
+module.exports = time;
+// es5/common js (ECMAScript 5)
+// es6 module
---
+++ test.js
@@ -0,0 +1,3 @@
+// require is used to import things using es5 module
+const res = require("./script")
+console.log(res)
