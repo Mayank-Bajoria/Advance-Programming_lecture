45db994df19995185ef5777d56a60b495b518a5a
mayankbajoria <bajoriamayank2007@gmail.com>
2026-08-19 09:45:52 +0530

    exporting function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const time  = '9 am';
-
-module.exports = time;
 // es5/common js (ECMAScript 5)
 // es6 module
+
+const time  = '9 am';
+// console.log(time)
+// console.log(time)
+// module.exports = time;
+const a = 10;
+console.log('Executing Script.js');
+console.log(a);
+let t = 1;
+function calc(){
+  return 9 + 1
+}
+const res = calc()
+module.exports = res
+
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 // require is used to import things using es5 module
 const res = require("./script")
-console.log(res)
+// const res1 = require("./script") // if we write any number of time this require thing it will not load again and again only one time it will relode there is no requirement of writing require more than 1 time for the same same file.
+// console.log(res)
