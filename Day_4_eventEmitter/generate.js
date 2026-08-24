5700a8e9324ee9cfde262a8e4076c1111749044f
mayankbajoria <bajoriamayank2007@gmail.com>
2026-08-24 10:13:17 +0530

    added a code of line in generate.js to make .csv file look good

--- Day_4_eventEmitter/generate.js
+++ Day_4_eventEmitter/generate.js
@@ -3,5 +3,5 @@ const fs = require("fs")
 fs.writeFileSync("big_data.csv","id,userName,orderValue\n");
 
 for(let i = 1; i <= 500_000; i++){
-  fs.appendFileSync("big_data.csv", `${i},username${i},${Math.random()*100}`)
+  fs.appendFileSync("big_data.csv", `${i},username${i},${Math.random()*100}\n`)
 }
