4ba95e48647c6b8239e9b85b90d0808d32b06018
mayankbajoria <bajoriamayank2007@gmail.com>
2026-08-24 09:35:08 +0530

    learning EventEmitter

---
+++ Day_4_eventEmitter/script.js
@@ -0,0 +1,15 @@
+// const events = require('events');
+// console.log(events);
+
+const EventEmitter = require("events");
+// console.log(events)
+// Promise..
+
+const celebrity1 = new EventEmitter();
+// const celebrity2 = new EventEmitter();
+
+celebrity1.on("release video" , () => {
+  console.log("Hey I am very happy and I am gonna watch it !!") // this is a listener so it should be written before emit command.
+})
+
+celebrity1.emit("release video") /// emit is used to fire an event
