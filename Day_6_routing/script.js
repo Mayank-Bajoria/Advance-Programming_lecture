53803fd973c51423260ddc780291160e1fa40ebc
mayankbajoria <bajoriamayank2007@gmail.com>
2026-08-31 10:06:16 +0530

    get id from users

--- Day_6_routing/script.js
+++ Day_6_routing/script.js
@@ -5,14 +5,29 @@ const{users,posts} = require("./data.js")
 
 const server = http.createServer((req,res) => {
 console.log(req.url)
+const path = req.url;
+const arr = path.split("/");
+const id = Number((arr[2]))
+
 console.log(req.method)
+
 res.writeHead(200, {
-  'Content-type': 'text/plain'
+  'Content-type': 'application/json'
 })
 if (req.url === '/users'){
   res.end(JSON.stringify(users));
 }
+else if (req.url === `/users/${id}`){
+  console.log('entered')
+  
+  res.end(JSON.stringify(users.find((obj) =>  obj.id == id)))
+}
 })
+
+
+
+
+
 
 server.listen(3001, () => {
   console.log("Server is listening at port 3001")
